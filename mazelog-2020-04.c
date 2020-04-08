65e4c96e9974779dd07065afbf3881891f718609
Christopher Wellons <wellons@nullprogram.com>
2020-04-08 17:56:58 -0400

    Add solver for 2020-04

---
+++ mazelog-2020-04.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define W 7
+#define H 5
+enum {NN, NE, EE, SE, SS, SW, WW, NW};
+static const char grid[] = {
+    SE, SE, EE, NN, EE, SE, SS,
+    SE, EE, EE, WW, WW, WW, EE,
+    SS, WW, WW, NN, WW, WW, EE,
+    NN, WW, NE, NN, WW, WW, EE,
+    SS, NN, WW, NN, SS, WW,  0
+};
+static const int moves[] = {
+    +0, -1, +1, -1, +1, +0, +1, +1, +0, +1, -1, +1, -1, +0, -1, -1
+};
+
+static int
+solve(int *p, int n, int bestn)
+{
+    if (p[n] == W * H - 1) {
+        for (int i = 0; i <= n; i++)
+            printf("%d%c", p[i] + 1, " \n"[i == n]);
+        bestn = n;
+    } else if (n < bestn - 1) {
+        int x = p[n] % W;
+        int y = p[n] / W;
+        int v = grid[p[n]];
+        if (n % 6 > 2) v = (v + 4) % 8;
+        for (int d = 1; ; d++) {
+            int xx = x + d * moves[v * 2 + 0];
+            int yy = y + d * moves[v * 2 + 1];
+            if (xx >= 0 && xx < W && yy >= 0 && yy < H) {
+                p[n + 1] = yy * W + xx;
+                bestn = solve(p, n + 1, bestn);
+            } else {
+                break;
+            }
+        }
+    }
+    return bestn;
+}
+
+int
+main(void)
+{
+    int path[23] = {0};
+    solve(path, 0, sizeof(path) / sizeof(*path));
+}
