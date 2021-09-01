cc9b8c67e5ca976c8ddaf1942616420fc56371d1
Christopher Wellons <wellons@nullprogram.com>
2021-09-01 17:19:32 -0400

    Add solver for 2021-09

---
+++ mazelog-2021-09.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+#define W 6
+#define H 6
+enum {P, R, B, K, Q, X};
+static const char grid[] = {
+    Q, B, K, B, K, P,
+    B, P, B, R, P, P,
+    K, B, R, B, K, P,
+    P, R, B, R, P, K,
+    P, B, R, B, P, B,
+    P, K, P, K, B, X,
+};
+static const signed char moves[] = {
+     0,  0, 10,  4, 18,  4, 26,  8, 42,  8,
+    -1, +0, +1, +0, +0, -1, +0, +1,
+    -1, -1, +1, +1, -1, +1, +1, -1,
+    -2, -1, -2, +1, +2, -1, +2, +1,
+    -1, -2, -1, +2, +1, -2, +1, +2,
+    -1, +0, +1, +0, +0, -1, +0, +1,
+    -1, -1, +1, +1, -1, +1, +1, -1,
+};
+
+static int
+solve(int *p, int n, int last, int bestn)
+{
+    if (grid[p[n]] == X) {
+        for (int i = 0; i <= n; i++)
+            printf("%d%c", p[i] + 1, " \n"[i == n]);
+        bestn = n;
+    } else if (n < bestn - 1) {
+        int x = p[n] % W;
+        int y = p[n] / W;
+        int c = grid[p[n]] ? grid[p[n]] : last;
+        int off = moves[c*2 + 0];
+        int len = moves[c*2 + 1];
+        const signed char *m = moves + off;
+        for (int i = 0; i < len; i++) {
+            int xx = x + m[i*2 + 0];
+            int yy = y + m[i*2 + 1];
+            if (xx >= 0 && xx < W && yy >= 0 && yy < H) {
+                p[n+1] = xx + yy*W;
+                int loop = 0;
+                if (grid[p[n+1]]) {
+                    for (int j = 0; !loop && j < n; j++) {
+                        if (p[j] == p[n+1]) {
+                            loop = 1;
+                        }
+                    }
+                }
+                if (!loop) {
+                    bestn = solve(p, n+1, c, bestn);
+                }
+            }
+        }
+    }
+    return bestn;
+}
+
+int
+main(void)
+{
+    int path[25] = {0};
+    solve(path, 0, 0, sizeof(path) / sizeof(*path));
+}
