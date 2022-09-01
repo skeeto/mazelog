c1a3b17cc5e1c0319f90b34d59579d3a9c6df34f
Christopher Wellons <wellons@nullprogram.com>
2022-09-01 14:48:45 -0400

    Add solver for 2022-09

---
+++ mazelog-2022-09.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define W 5
+#define H 5
+enum {NN, NE, EE, SE, SS, SW, WW, NW};
+enum {B=' ', R='@'};
+static const char grid[] = {
+    B|SS, B|SW, B|SS, R|SS, R|WW,
+    R|EE, B|EE, B|SS, B|NW, B|NW,
+    B|SS, R|EE, B|WW, B|NN, B|SS,
+    R|SE, B|NE, R|EE, R|NW, B|WW,
+    R|NE, B|NE, R|NE, R|NW, B|R,
+};
+static const int moves[] = {0,-1, 1,-1, 1,0, 1,1, 0,1, -1,1, -1,0, -1,-1};
+static const char opt[] = " @  @@   @@@    @@@@     @@@@@      @@@@@@";
+
+static int
+solve(int *p, int n, int bestn)
+{
+    if (p[n] == W*H - 1) {
+        for (int i = 0; i <= n; i++)
+            printf("%d%c", p[i] + 1, " \n"[i == n]);
+        bestn = n;
+    } else if (n < bestn - 1) {
+        int x = p[n] % W;
+        int y = p[n] / W;
+        int a = grid[p[n]] & 0x0f;
+        for (int d = 1; ; d++) {
+            int xx = x + d*moves[a*2 + 0];
+            int yy = y + d*moves[a*2 + 1];
+            if (xx >= 0 && xx < W && yy >= 0 && yy < H) {
+                int t = yy*W + xx;
+                if ((grid[t]&0xf0) & opt[n+1]) {
+                    p[n+1] = t;
+                    bestn = solve(p, n+1, bestn);
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    return bestn;
+}
+
+int
+main(void)
+{
+    int path[32] = {0};
+    solve(path, 0, sizeof(path)/sizeof(*path));
+}
