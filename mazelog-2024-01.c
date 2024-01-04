bb23127aa3d50efc362ad680fcd8d06fc6c619dc
Christopher Wellons <wellons@nullprogram.com>
2024-01-03 20:35:20 -0500

    Add solver for 2024-01

---
+++ mazelog-2024-01.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+enum {D=0x01, S=0x02, C=0x03, X=0x04};
+enum {B=0x10, G=0x20, V=0x30, Y=0x40, R=0x50};
+
+#define H 5
+#define W 5
+static const signed char grid[] = {
+    R|C, V|S, R|S, R|S, R|C,
+    Y|D, V|S, Y|D, G|S, Y|X,
+    B|C, B|S, G|C, Y|D, V|S,
+    G|X, V|X, G|X, Y|X, G|X,
+    R|S, Y|C, B|D, V|S, B|D,
+};
+static const signed char moves[] = {
+    -1, +0, +1, +0, +0, -1, +0, +1,
+    -1, -1, +1, +1, -1, +1, +1, -1,
+};
+
+static int solve(int *p, int n, int bestn)
+{
+    if (p[n] == W*H - 1) {
+        for (int i = 0; i <= n; i++) {
+            printf("%d%c", 1+p[i], i<n ? ' ' : '\n');
+        }
+        bestn = n;
+    } else if (n < bestn-1) {
+        int x = p[n] % W;
+        int y = p[n] / W;
+        for (int i = 0; i < 4; i++) {
+            for (int v = 1;; v++) {
+                int s = 8*(n/2 % 2);
+                int tx = x + v*moves[s + i*2 + 0];
+                int ty = y + v*moves[s + i*2 + 1];
+                if (tx>=0 && tx<W && ty>=0 && ty<H) {
+                    int t = ty*W + tx;
+                    int m = grid[p[n]] ^ grid[t];
+                    if (!(m&0xf0) || !(m&0x0f)) {
+                        p[n+1] = t;
+                        bestn = solve(p, n+1, bestn);
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    return bestn;
+}
+
+int main(void)
+{
+    int path[16] = {0};
+    solve(path, 0, sizeof(path)/sizeof(*path));
+    return 0;
+}
